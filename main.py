3fbe349a522e26fe0ac135d812c11ba9524cd5c8
jamesrivett <jrivett@students.kennesaw.edu>
2021-11-29 00:59:55 -0500

    first

---
+++ main.py
@@ -0,0 +1,16 @@
+from knapsack import *
+import random
+
+def createItemsList():
+    items = []
+    for i in range(30):
+        items.append([random.randint(1, 10), random.randint(1, 5)])
+    return items
+
+def main():
+    items = createItemsList()
+    print(items)
+        
+
+if __name__ == "__main__":
+    main()
