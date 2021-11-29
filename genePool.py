27c3e063030c79d77c697c1b8c81584e159c4265
jamesrivett <jrivett@students.kennesaw.edu>
2021-11-29 14:14:14 -0500

    added gene pool class

---
+++ genePool.py
@@ -0,0 +1,17 @@
+class genePool:
+    knapsacks = []
+
+    def __init__(self):
+        pass
+
+    def getKnapsacks(self):
+        return self.knapsacks
+
+    def add(self, item):
+        self.knapsacks.append(item)
+
+    def remove(self, index):
+        del self.knapsacks[index]
+
+    def clear(self):
+        self.knapsacks = []
